930cba86aaff5f190f3fe9824b2859a007fcdcbe
Yaakov <yaakovyitzchakbenmoshe@gmail.com>
2020-01-30 16:06:45 -0500

    a

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-﻿var coby = require("./COBY.js");
-var os = require("os");
+﻿var coby = require("./COBY.js")//,
+	//gm = require("gm");
+var os = require("os"),
+	Canvas = require("canvas");
+/*	can = Canvas.createCanvas(200,200),
+	ctx = can.getContext("2d");
+	
+	ctx.fillStyle = "#ff33aa";
+	ctx.fillRect(0,0,100,100);
+	ctx.fillStyle = "#eeff22";
+	ctx.fillRect(0,0,25,50);
+	var U = can.toDataURL(),
+		b = can.toBuffer();*/
 //console.log(coby)
 var pictures = [],
 	names = [],
 	yolo=0;
+	var gm = ()=>{}
 coby.adanServer({
 	port:771,
 	server: require("http").createServer(function(q,r) {
+		var url = q.
+					url.
+					split("/")
+					.filter(x=> x !== "");
+		
+		
 		r.setHeader("Access-Control-Allow-Origin","*");
 		/*console.log(names,pictures,JSON.stringify(pictures.map(x=> (
 						{
@@ -23,9 +41,13 @@ coby.adanServer({
 					r.end(data);
 				} else {
 			//		console.log("hi", yolo);
-					var p = pictures.find(x => x.id == q.url.replace("/", ""))
+					var p = pictures.find(x => x.id == url[0])
 					if(p) {
-						r.end(p.data);
+						if(url[1] == "gudal") { 
+							r.end(p.scaled)
+						} else {
+							r.end(p.data);
+						}
 					} else {
 						if(pictures.length > 0) {
 							r.end(JSON.stringify(pictures.map(x=> (
@@ -131,22 +153,38 @@ coby.adanServer({
 				}
 			}
 		});*/
-		pictures.push({
-			id:fileID,
-			data:m
-		});
-		yolo = Date.now() + "LOL";
-		if(cs.uploadInfo) {
-			cs.uploadInfo.files[0].fileID = fileID
-			if(cs.uploadInfo.uploadCallback) {
-				
-				cs.send({
-					[cs.uploadInfo.uploadCallback]: {
-						"got slice": cs.uploadInfo
-					}
-				})
+		var img = new Canvas.Image();
+		
+		img.onload = function() {
+			var can = Canvas.createCanvas(img.width, img.height),
+				ctx = can.getContext("2d");
+			var width = 300;
+			var height = width * img.height / img.width;
+			can.width = width;
+			can.height = height;
+			ctx.drawImage(img, 0, 0, width, height);
+			
+			var buff = can.toBuffer();
+			pictures.push({
+				id:fileID,
+				data:m,
+				scaled: buff
+			});
+			yolo = Date.now() + "LOL";
+			if(cs.uploadInfo) {
+				cs.uploadInfo.files[0].fileID = fileID
+				if(cs.uploadInfo.uploadCallback) {
+					
+					cs.send({
+						[cs.uploadInfo.uploadCallback]: {
+							"got slice": cs.uploadInfo
+						}
+					})
+				}
 			}
 		}
+		img.src = m;
+		
 	},
 	onOpen(c) {
 		console.log("WELCOME!");
